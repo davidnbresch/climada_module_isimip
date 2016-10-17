2792710111dc035d12f0a743fa282ee6c22b5cc7
davidnbresch <david.bresch@gmail.com>
2016-10-17 22:29:05 +0200

    ssp2_entity to work with shapes, too

--- code/isimip_ssp2_entity.m
+++ code/isimip_ssp2_entity.m
@@ -7,8 +7,18 @@ function entity=isimip_ssp2_entity(ISO3,ssp2_filename,NatId_filename,check_plot)
 % PURPOSE:
 %   create the entity based on isimip ssp (population) data
 %
-%   Programmer's note: later to be upgraded to work with a list of
-%   countries, in order to avoid loading all files each time...
+%   Checks the country ID (NatId) on NatId_filename and takes all gridcells
+%   within the requested country. If the resolution of the NetId does not
+%   match the population data or if there is no NatId_filename provided,
+%   the code uses the climada country shape files to select the gridcells
+%   within the country (the code notifies).
+%
+%   The asset values are then scaled by GDP and the income group, see
+%   climada_entity_value_GDP_adjust_one.
+%
+%   Programmer's note: While the present code works for a list or even all
+%   countries, it shall later to be upgraded to work more efficiently with
+%   a list of countries, in order to avoid loading all files each time...  
 %
 %   Octave: please install the netCDF package first:
 %   pkg install -forge netcdf -local -auto
@@ -27,14 +37,16 @@ function entity=isimip_ssp2_entity(ISO3,ssp2_filename,NatId_filename,check_plot)
 %   ssp2_filename: filename of the .nc file with the population values
 %   NatId_filename: filename of the .nc file with the national grid IDs (to
 %       assign grid cells to countries). Default set in PARAMETERS
+%       To avoid using NatId and just use the grid cells within a country
+%       shape, set NatId_filename='ignore'.
 %   check_plot: whether show a check plot (=1, default), or not (=0)
-%       Note that plotting might often take longer than the full
-%       conversion...
-%       if =2, also show the centroids as red dots
-%       if =3, also show the original data grid as blue dots (might take time...)
 % OUTPUTS:
 %   entity: a climada entity structure, see climada_entity_read for a full
 %       description of all fields
+%       PLUS the field entity.assets.Values(n_times,n_centroids) with the
+%       variable as on the ssp2 file for each timestep at each centroid.
+%       These values are NOT scaled (different from entity.assets.Value,
+%       which is scaled by GDP and income group).   
 % MODIFICATION HISTORY:
 % David N. Bresch, david.bresch@gmail.com, 20161017, initial
 %-
@@ -49,9 +61,11 @@ if ~climada_init_vars,return;end % init/import global variables
 if ~exist('ISO3','var'),          ISO3           = '';end
 if ~exist('ssp2_filename','var'), ssp2_filename  = '';end
 if ~exist('NatId_filename','var'),NatId_filename = '';end
-if ~exist('check_plot','var'),    check_plot     =  1;end
+if ~exist('check_plot','var'),    check_plot     =  0;end
 
 % PARAMETERS
+%
+verbose=1; % default=1, to suppress output to stdout later
 %
 % define the defaut folder for isimip TC track data
 isimip_data_dir=[climada_global.data_dir filesep 'isimip'];
@@ -66,6 +80,9 @@ if isempty(NatId_filename),NatId_filename=[isimip_data_dir filesep 'Nat_id_grid_
 %
 % the entity template to populate a defaiult entity
 entity_template=[climada_global.entities_dir filesep 'entity_template'];
+%
+% admin0 shape file (for fallback selection option):
+admin0_shape_file=climada_global.map_border_file;
 %
 % define the isimip country ISO3 code list
 ISO3_list={
@@ -303,12 +320,12 @@ ISO3_list={
 
 if strcmpi(ISO3,'all')
     % process all
-    fprintf('processing %i entities\n',length(ISO3_list));
+    if verbose,fprintf('processing %i entities\n',length(ISO3_list));end
     for iso3_i=1:length(ISO3_list)
         ISO3=ISO3_list(iso3_i,1);
         entity=isimip_ssp2_entity(ISO3,ssp2_filename,NatId_filename,check_plot);
     end
-    fprintf('only last entity returned, see climada_entity_load\n');
+    if verbose,fprintf('only last entity returned, see climada_entity_load\n');end
 end
 
 if isempty(ISO3)
@@ -329,53 +346,108 @@ if ~isempty(iso3_pos)
     NatId=ISO3_list{iso3_pos,2}; % get the ID
 end % ~isempty(iso3_pos)
 
-% read the NatId grid
-nc.NatIdGrid = ncread(NatId_filename,'NatIdGrid');
+% read the population data
+if verbose,fprintf('reading lon, lat, time and var1 from %s ...',ssp2_filename);end
+% if troubles, use ncinfo(flood_fraction_filename,'var') ...
+nc.lon       = ncread(ssp2_filename,'lon');
+nc.lat       = ncread(ssp2_filename,'lat');
+nc.time      = ncread(ssp2_filename,'time');
+nc.var1      = ncread(ssp2_filename,'var1');
+if verbose,fprintf(' done\n');end
 
-NatId_pos=find(nc.NatIdGrid == NatId);
+% create the grid
+if verbose,fprintf('creating regular grid (meshgrid) ...');end
+[gridlon,gridlat] = meshgrid(nc.lon,nc.lat);
+% reform to 1D list
+gridlon=gridlon';
+gridlat=gridlat';
+gridlon=reshape(gridlon,[1 numel(gridlon)]);
+gridlat=reshape(gridlat,[1 numel(gridlat)]);
+if verbose,fprintf(' done\n');end
 
-if ~isempty(NatId_pos) % there are gridcells within this country
+NatId_pos=[]; % init
+if exist(NatId_filename,'file')
+    % read the NatId grid
+    nc.NatIdGrid = ncread(NatId_filename,'NatIdGrid');
     
-    % rewad the population data
-    fprintf('reading lon, lat, time and var1 from %s ...',ssp2_filename);
-    % if troubles, use ncinfo(flood_fraction_filename,'var') ...
-    nc.lon      = ncread(ssp2_filename,'lon');
-    nc.lat      = ncread(ssp2_filename,'lat');
-    nc.time     = ncread(ssp2_filename,'time');
-    nc.var1     = ncread(ssp2_filename,'var1');
-    fprintf(' done\n');
-    
-    if sum(size(nc.var1(:,:,1))-size(nc.NatIdGrid)) == 0
-        % grid sizes the same
-        
-        % get template entity
-        entity=climada_entity_load(entity_template);
-        
-        entity_filename=[climada_global.entities_dir filesep ISO3 '_entity'];
-        
-        % create the grid
-        [gridlon,gridlat] = meshgrid(nc.lon,nc.lat);
-        
-        entity.assets.lon=gridlon(NatId_pos);
-        entity.assets.lat=gridlat(NatId_pos);
-        for time_i=1:length(nc.time)
-            temp_var1=var1(NatId_pos,time_i);
-            entity.assets.Value=temp_var1(NatId_pos);
-        end  % time_i
-        
-        % complete entity
-        entity.assets.Cover=entity.assets.Value;
-        entity.assets.Deductible=entity.assets.Value*0;
-        entity.assets.DamageFunID=entity.assets.Deductible+1;
-        
-        %fprintf('saving entity as %s\n',entity_filename)
-        %save(entity_filename,'entity');
-        
-        if check_plot,climada_entity_plot(entity);end
-        
+    if sum(abs(size(nc.var1(:,:,1))-size(nc.NatIdGrid))) == 0 % grid sizes the same
+        NatIdGrid=reshape(nc.NatIdGrid,[1 numel(nc.NatIdGrid)]);
+        NatId_pos=find(NatIdGrid == NatId);
+        n_centroids=length(NatId_pos);
+        if verbose,fprintf('%i NatId grid cells within country %s\n',n_centroids,ISO3);end
     else
-        fprintf('ERROR: grid sizes do not match, aborted\n');
+        fprintf('Warning: grid sizes do not match, 2nd approach (shape)\n');
     end
+else
+    fprintf('Warning: NatIdGrid file not found, 2nd approach (shape)\n');
+end
+
+if isempty(NatId_pos) % 2nd try, using shapes
+    
+    % read admin0 (country) shape file
+    admin0_shapes=climada_shaperead(admin0_shape_file);
+    
+    % figure the country shape
+    shape_i=[];
+    for shape_ii=1:length(admin0_shapes)
+        if strcmpi(admin0_shapes(shape_ii).ADM0_A3,ISO3),shape_i=shape_ii;end
+    end % shape_ii
+    
+    if ~isempty(shape_i)
+        NatId_pos=climada_inpolygon(gridlon,gridlat,admin0_shapes(shape_i).X,admin0_shapes(shape_i).Y,0);
+        n_centroids=sum(NatId_pos);
+        if n_centroids>0
+            if verbose,fprintf('%i grid cells within country %s shape\n',n_centroids,ISO3);end
+        else
+            fprintf('ERROR: no gridpoints within shape, aborted\n');
+            return
+        end
+    else
+        fprintf('ERROR: %s not in shape file, aborted\n',ISO3);
+    end
+end
+
+if ~isempty(NatId_pos)
+    
+    % get template entity
+    entity=climada_entity_load(entity_template);
+    entity_filename=[climada_global.entities_dir filesep ISO3 '_entity'];
+    
+    entity.assets.admin0_ISO3=ISO3;
+    
+    entity.assets.lon=gridlon(NatId_pos);
+    entity.assets.lat=gridlat(NatId_pos);
+    
+    n_times=length(nc.time);
+    if verbose,fprintf('extracting %i centroids at %i times ...',n_centroids,n_times);end
+    entity.assets.Values=zeros(n_times,n_centroids);
+    for time_i=1:n_times
+        temp_var1=reshape(nc.var1(:,:,time_i),[1 numel(nc.var1(:,:,time_i))]); % get time slice
+        entity.assets.Values(time_i,:)=temp_var1(NatId_pos);
+    end  % time_i
+    entity.assets.Value=entity.assets.Values(1,:); % copy first one
+    if verbose,fprintf(' done\n');end
+    
+    % complete entity
+    entity.assets.Cover      =entity.assets.Value;
+    entity.assets.Deductible =entity.assets.Value*0;
+    entity.assets.DamageFunID=entity.assets.Deductible+1;
+    entity.assets.Category_ID=entity.assets.DamageFunID;
+    entity.assets.Region_ID  =entity.assets.DamageFunID;
+    
+    entity.assets = rmfield(entity.assets,'centroid_index');
+    entity.assets = rmfield(entity.assets,'Value_unit');
+    entity.assets = rmfield(entity.assets,'hazard');
+    entity.assets = climada_assets_complete(entity.assets);
+    
+    % scale up asset values based on a country's estimated total asset value
+    entity=climada_entity_value_GDP_adjust_one(entity,verbose);
+    entity.assets.isimip_comment='only entity.assets.Value scaled, entity.assets.Values not modified';
+    
+    if verbose,fprintf('saving entity as %s\n',entity_filename);end
+    %save(entity_filename,'entity');
+    
+    if check_plot,climada_entity_plot(entity);end
     
 end % ~isempty(NatId_pos)
 
