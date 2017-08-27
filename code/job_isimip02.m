ab02f93a8ed6ca06e847ed064b027edfe30abc01
davidnbresch <david.bresch@gmail.com>
2017-08-27 18:36:32 +0200

    batch-jobs updated
    from now on, we always list the last time executed on the cluster.
    Check plots added to 01

--- code/job_isimip02.m
+++ code/job_isimip02.m
@@ -1,63 +1,90 @@
-% batch job for cluster: bsub -R "rusage[mem=5000]" -n 24 matlab -nodisplay -singleCompThread -r job_isimip01
+% batch job for cluster: bsub -R "rusage[mem=5000]" -n 24 matlab -nodisplay -singleCompThread -r job_isimip02
 % MODULE:
 %   isimip
 % NAME:
-%   job_isimip01
+%   job_isimip02
 % PURPOSE:
 %   generate isimip tropical cyclone (TC) hazard event sets based on Kerry
-%   Emmanuel TC track files
+%   Emmanuel TC track files for 2nd batch, i.e. files such as
+%   Trial1_GB_dkgfdl_20thcal.mat
 %
-%   See PARAMETERS before running this
+%   See PARAMETERS before running this and run SPECIAL CODE (also below) in
+%   order to prepare the lostz of track files in ascending size order
 %
 %   Once all runs are done, consider using SPECIAL CODE2 at the end below
 %   to create check plots
 %
-%   some hints to work with the cluster (explicit paths, edit this ;-)
-%   copy job to cluster:    scp -r Documents/_GIT/euler_jobs/job_isimip01b.m dbresch@euler.ethz.ch:/cluster/home/dbresch/euler_jobs/.
-%   copy job logfiles back: scp -r dbresch@euler.ethz.ch:/cluster/scratch/dbresch/climada_data/hazards/*.mat Documents/_GIT/climada_data/hazards/.
-%   copy results back:  scp -r dbresch@euler.ethz.ch:
 % CALLING SEQUENCE:
-%   bsub -R "rusage[mem=5000]" -n 24 matlab -nodisplay -singleCompThread -r job_isimip01
+%   bsub -R "rusage[mem=5000]" -n 24 matlab -nodisplay -singleCompThread -r job_isimip02
 % EXAMPLE:
-%   bsub -R "rusage[mem=5000]" -n 24 matlab -nodisplay -singleCompThread -r job_isimip01
+%   bsub -R "rusage[mem=5000]" -n 24 matlab -nodisplay -singleCompThread -r job_isimip02
 % INPUTS:
 % OPTIONAL INPUT PARAMETERS:
 % OUTPUTS:
 %   to scratch disk, see PARAMETERS
 % MODIFICATION HISTORY:
-% David N. Bresch, dbresch@ethz.ch, 20170403
-% David N. Bresch, dbresch@ethz.ch, 20170704, just checked
-% David N. Bresch, dbresch@ethz.ch, 20170705, cluster hints added
-% David N. Bresch, dbresch@ethz.ch, 20170827, last executed
+% David N. Bresch, dbresch@ethz.ch, 20170825, copy from job_isimip01
+% David N. Bresch, dbresch@ethz.ch, 20170826, last executed
 %-
 
 % PARAMETERS
 % one should only have to edit this section
 cd /cluster/home/dbresch/climada % to make sure the cluster finds climada
 scratch_dir = '/cluster/scratch/dbresch/climada_data/hazards';
+%scratch_dir = '/Users/bresch/Documents/_GIT/climada_data/isimip/scratch'; % for local tests
 %
-% the list of TC track files to be processed
+% the list of TC track files to be processed (see SPECIAL CODE below)
 track_files={
-    'temp_miroc20thcal' % all tc_track files in ascending size
-    'temp_ccsm420thcal'
-    'temp_gfdl520thcal'
-    'temp_hadgem20thcal'
-    'temp_mri20thcal'
-    'temp_mpi20thcal'
-    'temp_mirocrcp85cal_full'
-    'temp_ccsm4rcp85_full'
-    'temp_hadgemrcp85cal_full'
-    'temp_mrircp85cal_full'
-    'temp_gfdl5rcp85cal_full'
-    'temp_mpircp85cal_full'
+    'Trial1_GB_dkmiroc_20thcal'
+    'Trial1_GB_dkhad_20thcal'
+    'Trial1_GB_dkipsl_20thcal'
+    'Trial1_GB_dkgfdl_20thcal'
+    'Trial1_GB_dkmiroc_piControlcal'
+    'Trial1_GB_dkipsl_piControlcal'
+    'Trial1_GB_dkgfdl_piControlcal'
+    'Trial1_GB_dkmiroc_rcp26cal'
+    'Trial2_GB_dkgfdl_20thcal'
+    'Trial1_GB_dkmiroc_rcp60cal'
+    'Trial1_GB_dkgfdl_rcp60cal'
+    'Trial1_GB_dkgfdl_rcp26cal'
+    'Trial1_GB_dkhad_rcp26cal'
+    'Trial1_GB_dkhad_rcp60cal'
+    'Trial2_GB_dkmiroc_20thcal'
+    'Trial1_GB_dkipsl_rcp60cal'
+    'Trial1_GB_dkipsl_rcp26cal'
+    'Trial2_GB_dkmiroc_rcp26cal'
+    'Trial2_GB_dkipsl_rcp26cal'
     };
+%
+% % SPECIAL CODE to sort track files by size (run this on command line to
+% % obtain above list). Ascending size helps to run until we encounter
+% % memory problems and can then tackle them (if needed)
+% % ---------------------------------------------------------------------
+% dd_name={};dd_bytes=[]; % init
+% tc_tracks_dir=[climada_global.data_dir filesep 'isimip/tc_tracks'];
+% dd=dir(tc_tracks_dir);
+% for i=1:length(dd)
+%     if dd(i).isdir && length(dd(i).name)>2
+%         check_file=[tc_tracks_dir filesep dd(i).name filesep dd(i).name '.mat'];
+%         ddd=dir(check_file);
+%         dd_name{end+1}=dd(i).name;
+%         dd_bytes(end+1)=ddd(1).bytes;
+%     end % dd(i).isdir
+% end % i
+% [~,pos] = sort(dd_bytes);
+% dd_bytes=dd_bytes(pos);
+% dd_name=dd_name(pos);
+% fprintf('track files sorted ascening by size (%2.2g..%2.2g bytes):\n',dd_bytes(1),dd_bytes(end));
+% for i=1:length(dd_name),fprintf('''%s''\n',dd_name{i});end
+% fprintf('--> copy paste this into track_files in %s\n','job_isimip02');
+
 
 startup % climada_global exists afterwards
 pwd % just to check where the job is running from
 N_pool_workers=24; % for parpool
 climada_global.parfor=1; % for parpool
 
-% perpare centroids (full globe leads to segmentation fault)
+% prepare centroids (full globe leads to segmentation fault)
 centroids_S=climada_centroids_load('GLB_NatID_grid_0360as_adv_1');
 
 centroids_N = centroids_S; % Northern hemisphere
@@ -96,7 +123,7 @@ delete(pool)
 %
 % % SPECIAL CODE2 to inspect results
 % % ----------------------------------
-% result_hazard_dir='/Users/bresch/polybox/isimip/hazards_v01';
+% result_hazard_dir='/Users/bresch/polybox/isimip/hazards_v02';
 % fig_dir='/Users/bresch/Desktop/isimip';fig_ext='png';
 % if ~isdir(fig_dir),[fP,fN]=fileparts(fig_dir);mkdir(fP,fN);end % create it
 % dd=dir([result_hazard_dir filesep '*.mat']);
