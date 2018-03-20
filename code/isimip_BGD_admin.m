997935c7cf10e38683978ed88d4056b94e27c760
davidnbresch <david.bresch@gmail.com>
2018-03-20 17:56:39 +0100

    ignore .mat file of shape

--- code/isimip_BGD_admin.m
+++ code/isimip_BGD_admin.m
@@ -52,7 +52,7 @@ module_data_dir=[fileparts(fileparts(mfilename('fullpath'))) filesep 'data'];
 %
 % set default shape file name
 if isempty(shapefile_name),shapefile_name=[module_data_dir filesep 'centroids' filesep 'bgd_bnda_adm1_2015/bgd_bnda_adm1_2015.shp'];end
-shapefile_name='/Users/bresch/Documents/_GIT/climada_applications_div/isimip_BGD/admin_layers/bgd_bnda_adm1_2015/bgd_bnda_adm1_2015.shp';
+%shapefile_name='/Users/bresch/Documents/_GIT/climada_applications_div/isimip_BGD/admin_layers/bgd_bnda_adm1_2015/bgd_bnda_adm1_2015.shp';
 %
 plot_colors={'.r','.g','.b','.k','.m','.y'};
 
